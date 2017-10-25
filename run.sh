c6c6d6701e725fda91c9610e91b402b63549cd8e
Vadim Homchik <homchik@gmail.com>
2017-10-25 16:09:14 +0300

    use cert not base64 encoded

--- run.sh
+++ run.sh
@@ -35,7 +35,7 @@ fi
 
 kubectl config set-credentials default --token=$KUBERNETES_TOKEN
 if [ ! -z $KUBERNETES_CERT ]; then
-  echo $KUBERNETES_CERT | base64 -d > ca.crt
+  echo $KUBERNETES_CERT > ca.crt
   kubectl config set-cluster default --server=$KUBERNETES_SERVER --certificate-authority=ca.crt
 else
   echo "WARNING: Using insecure connection to cluster"
